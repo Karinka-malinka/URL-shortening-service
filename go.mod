93be7500ef2500119991bb3fb9b50b54de51b161
Karinka-malinka <k.malutina@unistar.ru>
2023-12-25 17:26:32 +0300

    iter 3

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/URL-shortening-service
 go 1.21.3
 
 require (
+	github.com/go-chi/chi/v5 v5.0.11
 	github.com/speps/go-hashids v2.0.0+incompatible
 	github.com/stretchr/testify v1.8.4
 )
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,15 +6,54 @@ import (
 	"strings"
 
 	"github.com/URL-shortening-service/internal/app"
+	"github.com/go-chi/chi/v5"
 )
 
 // функция run будет полезна при инициализации зависимостей сервера перед запуском
 func Run() error {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc(`/`, mainHandler)
+	r := chi.NewRouter()
 
-	return http.ListenAndServe(`:8080`, mux)
+	r.Get("/{id}", getURL)
+	r.Post("/", shorteningURL)
+
+	return http.ListenAndServe(`:8080`, r)
+}
+
+func getURL(w http.ResponseWriter, r *http.Request) {
+
+	longURL := app.GetURL(chi.URLParam(r, "id"))
+	if longURL == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	http.Redirect(w, r, longURL, http.StatusTemporaryRedirect)
+}
+
+func shorteningURL(w http.ResponseWriter, r *http.Request) {
+
+	if r.Body == http.NoBody {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	if len(body) == 0 {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	urlShort := "http://" + r.Host + "/" + app.ShorteningURL(string(body))
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(urlShort))
 }
 
 // функция webhook — обработчик HTTP-запроса
