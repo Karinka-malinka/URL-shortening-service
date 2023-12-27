4c5ba718f0bc6e5b8b3d87943756cd19c2cbfc31
Karinka-malinka <k.malutina@unistar.ru>
2023-12-27 15:20:03 +0300

    bug fix

--- cmd/shortener/flag.go
+++ cmd/shortener/flag.go
@@ -9,7 +9,7 @@ import (
 func parseFlags(cfg *config.ConfigData) {
 
 	flag.StringVar(&cfg.RunAddr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&cfg.ShortAddr, "b", "http://localhost:8000", "base address of the resulting shortened URL")
+	flag.StringVar(&cfg.ShortAddr, "b", "http://localhost:8080", "base address of the resulting shortened URL")
 
 	// парсим переданные серверу аргументы в зарегистрированные переменные
 	flag.Parse()
